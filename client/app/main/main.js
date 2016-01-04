d9ff43f1181bc9e040c1c08761bf6ba8628b42a4
ashwin pote <ashwin.pote@globant.com>
2016-01-04 12:47:03 +0530

    added required files for project

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('apiIntegrationApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
