cdbb4adfa287d138f7e5a6d029d6c068698fe078
mugdha sonawane <mugdha.sonawane@globant.com>
2016-01-19 17:26:18 +0530

    added drag drop functionality

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('apiIntegrationApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngDragDrop'
 ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider
