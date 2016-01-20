4afece426fa175202b26b800a3b0f94673efd9c1
ashwin pote <ashwin.pote@globant.com>
2016-01-20 12:43:57 +0530

    updated main controller js file

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,16 +1,15 @@
 'use strict';
-
 (function() {
     class MainController {
         constructor(googleplus, twitter, $scope, $window) {
             var currSelect = "";
-            $scope.showInputControls = false;            
+            $scope.showInputControls = false;
 
             $scope.onChanged = function(param) {
                 currSelect = $scope.optionValue;
                 $scope.trends = "";
                 $scope.result = "";
-                
+
                 switch (currSelect) {
                     case "Google+":
                         googleplus.init().then(function() {
@@ -19,7 +18,7 @@
                         break;
                     case "Twitter":
                         twitter.init();
-                        $scope.showInputControls = true;                        
+                        $scope.showInputControls = true;
                         break;
                 }
             }
@@ -28,7 +27,6 @@
                 switch (currSelect) {
                     case "Google+":
                         googleplus.search($scope.trends).then(function(data) {
-                            console.log(data.items)
                             $scope.result = data.items;
                         });
                         break;
@@ -39,13 +37,12 @@
                         break;
                 }
             }
-            $scope.removeField = function(param){
-                $scope.result.splice(param,1);  
+            $scope.removeField = function(param) {
+                $scope.result.splice(param, 1);
             }
         }
     }
 
     angular.module('apiIntegrationApp')
         .controller('MainController', MainController);
-
 })();
