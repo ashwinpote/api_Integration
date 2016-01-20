71e207b901e46ec94f11ad2957127cd36bcea4d8
ashwin pote <ashwin.pote@globant.com>
2016-01-20 11:17:41 +0530

    modified google plus api code

--- client/app/googleplus/googleplus.service.js
+++ client/app/googleplus/googleplus.service.js
@@ -2,18 +2,21 @@
 
 angular.module('apiIntegrationApp')
     .service('googleplus', function($q) {
-    	var deferred = $q.defer();
+        var deferred = $q.defer();
+        var auth_user = false;
         var obj = {}
         var clientId = '266935583518-0otsvqdm1eekgr9htkca8lfla26k1l90.apps.googleusercontent.com',
             apiKey = 'AIzaSyAnMX4jucCO6omqJLTUZ4lkqZtDUY_cX2o',
             scopes = 'https://www.googleapis.com/auth/plus.me';
 
         obj.init = function() {
-            gapi.auth.authorize({
-                client_id: clientId,
-                scope: scopes,
-                immediate: false
-            }, obj.handleAuthResult);
+            if (!auth_user) {
+                gapi.auth.authorize({
+                    client_id: clientId,
+                    scope: scopes,
+                    immediate: false
+                }, obj.handleAuthResult);               
+            }
             return deferred.promise;
         }
 
@@ -27,7 +30,7 @@ angular.module('apiIntegrationApp')
             gapi.auth.authorize({
                 client_id: clientId,
                 scope: scopes,
-                immediate: true,
+                immediate: false,
                 hd: domain
             }, obj.handleAuthResult);
         };
@@ -39,10 +42,11 @@ angular.module('apiIntegrationApp')
                     var request = gapi.client.oauth2.userinfo.get();
                     request.execute(function(resp) {
                         //console.log(resp)
+                        auth_user = true;
                     });
                     deferred.resolve(data);
                 });
-               
+
             } else {
                 deferred.reject('error');
             }
@@ -59,11 +63,16 @@ angular.module('apiIntegrationApp')
         };
 
         obj.search = function(q) {
-        	var deferred = $q.defer();
+            var deferred = $q.defer();
             var search = encodeURIComponent(q);
             var request = gapi.client.request({
                 'path': '/plus/v1/activities',
-  				'params': {'query': ""+search+"", 'orderBy': 'best','sortBy':'recent', 'maxResults':'20'}
+                'params': {
+                    'query': "" + search + "",
+                    'orderBy': 'best',
+                    'sortBy': 'recent',
+                    'maxResults': '20'
+                }
             });
             request.then(function(resp) {
                 deferred.resolve(resp.result);
