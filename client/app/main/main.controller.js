ef4e1a081b11f6454781e8380c6c2a9ef57bcbde
ashwin pote <ashwin.pote@globant.com>
2016-01-20 17:37:04 +0530

    added showing div functionality in main html

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,12 +3,12 @@
     class MainController {
         constructor(googleplus, twitter, $scope, $window) {
             var currSelect = "";
+            var currArr = [];
             $scope.showInputControls = true;
 
             $scope.onChanged = function(param) {
                 currSelect = $scope.optionValue;
                 $scope.trends = "";
-                $scope.result = "";
             }
 
             $scope.search = function() {
@@ -16,16 +16,23 @@
                     case "Google+":
                         googleplus.init().then(function(data) {
                             googleplus.search($scope.trends).then(function(data) {
+                                currArr.push(data.items);
                                 $scope.result = data.items;
+                                $scope.mainresult = currArr;
+                                console.log($scope.result)
+                                 console.log($scope.mainresult)
                             });
                         })
                         break;
                     case "Twitter":
                         twitter.search($scope.trends).then(function(data) {
-                            $scope.result = data;
+                            currArr.push(data);
+                            $scope.result = currArr;
+                            console.log(currArr);
                         });
                         break;
                 }
+
             }
             $scope.removeField = function(param) {
                 $scope.result.splice(param, 1);
